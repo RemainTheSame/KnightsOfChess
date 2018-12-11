42b3dcc12b0b2a235ff048920527ab531a095df4
RemainTheSame <halooscur.orava@gmail.com>
2018-12-11 17:13:11 +0100

    Main Menu Text added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,25 @@ package sample;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -35,15 +42,55 @@ public class Main extends Application {
         //Sets title and current scene as main menu
         primaryStage.setTitle("Knights Of Chess");
         Scene mainMenu = new Scene(root, 1080, 720);
+        //String css = this.getClass().getResource("/Stylesheets/mainstyle.css").toExternalForm();
+        mainMenu.getStylesheets().add("/Stylesheets/mainstyle.css");
         //Scene newGame = new Scene(root, 1080,720);
         primaryStage.setScene(mainMenu);
 
-        //Create and add canvas to root group
-        Canvas canvas = new Canvas(1080, 720);
+        //Create pane and vertical box
         Pane mainPane = new Pane();
-        //root.getChildren().add(canvas);
+        VBox menuBox = new VBox();
+
+        //Setup labels with font
+        //Font menuFont = new Font(50);
+        Label quit = new Label("Quit");
+        Label newGame = new Label("New Game");
+        //quit.setFont(menuFont);
+        //newGame.setFont(menuFont);
+
+        //Add stylesheet to labels
+        quit.getStyleClass().add("menuLabel");
+        newGame.getStyleClass().add("menuLabel");
+
+
+        //Add listeners to labels
+        quit.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);
+            }
+        });
+        newGame.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("New Game clicked");
+            }
+        });
+
+        //Configure menuBox
+        menuBox.setSpacing(30);
+        //Adds the labels
+        menuBox.getChildren().addAll(newGame,quit);
+        menuBox.setLayoutY(300);
+        menuBox.setLayoutX(300);
+
+        //Add menuBox to the Pane
+        mainPane.getChildren().add(menuBox);
+
+        //Add mainPane to root group
         root.getChildren().add(mainPane);
 
+        //General onKeyPress and Release
         ArrayList<String> input = new ArrayList<>();
 
         mainMenu.setOnKeyPressed(
@@ -58,7 +105,6 @@ public class Main extends Application {
                     }
                 }
         );
-
         mainMenu.setOnKeyReleased(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
@@ -67,58 +113,16 @@ public class Main extends Application {
             }
         });
 
-
-        //Text setup for canvas
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Font mainFont = Font.font(50);
-        gc.setFont(mainFont);
-        Text newGame = new Text("New Game");
-        Text quitGame = new Text( "Quit");
-        Font textFont = new Font(50);
-        newGame.setFont(textFont);
-        quitGame.setFont(textFont);
-        //TextFlow newFlow = new TextFlow(newGame);
-        //TextFlow quitFlow = new TextFlow(quitGame);
-        //quitFlow.setLayoutX(200);
-        //quitFlow.setLayoutY(300);
-        //newFlow.setLayoutX(200);
-        //newFlow.setLayoutY(200);
-        //mainPane.getChildren().addAll(newFlow, quitFlow);
-
-        Label quitLabel = new Label("Quit");
-        Label newLabel = new Label("New Game");
-
-        mainPane.getChildren().addAll(quitLabel, newLabel);
-        quitLabel.setLayoutX(200);
-        newLabel.setLayoutX(300);
-        newLabel.setLayoutY(300);
-        quitLabel.setLayoutY(200);
-
-
-
-        /*
-        //gc text
-        gc.fillText(newGame.getText(),100,100);
-        gc.fillText(quitGame.getText(), 100, 200);
-        */
-
-
-        //Mouse click options
+        //General mouse click options
         mainMenu.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 //Check if mouse click is within a objects area
                 //if(targetData.containsPoint(x,y))...
-
-                if(newLabel.contains(event.getX(), event.getY())){
-                    System.out.println("New Game pressed");
-                }
-                if(quitLabel.contains(event.getX(), event.getY())){
-                    System.out.println("Quit pressed");
-                }
             }
         });
 
+        //Game loop setup
         final long startNanoTime = System.nanoTime();
         //Handle occurs 60 times per second
         new AnimationTimer(){
