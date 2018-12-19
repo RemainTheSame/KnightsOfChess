cc274c2f9ec0f0d475d20c4bc64b8766f3345bf9
RemainTheSame <halooscur.orava@gmail.com>
2018-12-19 21:20:07 +0100

    Documentation class added

---
+++ src/sample/ProjectDocumentation.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class ProjectDocumentation {
+
+    //DOCUMENTATION CLASS
+
+    //Design Patterns:
+    /*
+    * Decorator - Adding functionality to Player/NPC
+    * Example.
+    * Player chooses a new class, giving them new abilities.
+    *
+    * Player dual-classes, giving two classes abilities
+    *
+    * Factory - Simplify creation of different NPCs
+    * Example.
+    * We have a standard Knight NPC and standard Rook npc
+    * XNPC = Factory.createKnight()/createRook();
+    *
+    *
+    *
+    * 
+    * */
+
+}
